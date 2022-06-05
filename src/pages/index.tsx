36771e7dc4969c7d484c13d3cf51dfdfb2d128a9
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-06-05 18:30:43 -0300

    Terminando configuração do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -37,7 +37,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ( ) => {
+export const getStaticProps: GetStaticProps = async ( ) => {
   const price = await stripe.prices.retrieve('price_1L6h7zDXjs2qds7y2ahf2zaa');
  
   const product = {
@@ -52,7 +52,9 @@ export const getServerSideProps: GetServerSideProps = async ( ) => {
 
   return {
     props: {
-      product
-    }
+      product,
+    },
+
+    revalidate: 60 * 60 * 24, //24 horas
   }
 }
